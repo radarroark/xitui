437be3fdca368cc0188e589ee297c854ec2d94f2
radar roark <122068506+radarroark@users.noreply.github.com>
2024-01-10 17:15:37 -0500

    track parent id of focus rects

--- src/focus.zig
+++ src/focus.zig
@@ -7,7 +7,12 @@ pub const Focus = struct {
     id: usize,
     child_id: ?usize,
     focusable: bool,
-    rects: std.AutoHashMap(usize, layout.URect),
+    children: std.AutoHashMap(usize, Child),
+
+    const Child = struct {
+        parent_id: usize,
+        rect: layout.URect,
+    };
 
     pub fn init(allocator: std.mem.Allocator) Focus {
         const id = next_id;
@@ -16,26 +21,32 @@ pub const Focus = struct {
             .id = id,
             .child_id = null,
             .focusable = false,
-            .rects = std.AutoHashMap(usize, layout.URect).init(allocator),
+            .children = std.AutoHashMap(usize, Child).init(allocator),
         };
     }
 
     pub fn deinit(self: *Focus) void {
-        self.rects.deinit();
+        self.children.deinit();
     }
 
     pub fn addChild(self: *Focus, child: *const Focus, size: layout.Size, target_x: usize, target_y: usize) !void {
         if (child.focusable) {
-            try self.rects.put(child.id, .{ .x = target_x, .y = target_y, .size = size });
+            try self.children.put(child.id, .{
+                .parent_id = self.id,
+                .rect = .{ .x = target_x, .y = target_y, .size = size },
+            });
         }
-        var iter = child.rects.iterator();
+        var iter = child.children.iterator();
         while (iter.next()) |entry| {
-            const rect = entry.value_ptr.*;
-            try self.rects.put(entry.key_ptr.*, .{ .x = target_x + rect.x, .y = target_y + rect.y, .size = rect.size });
+            const grandchild = entry.value_ptr.*;
+            try self.children.put(entry.key_ptr.*, .{
+                .parent_id = grandchild.parent_id,
+                .rect = .{ .x = target_x + grandchild.rect.x, .y = target_y + grandchild.rect.y, .size = grandchild.rect.size },
+            });
         }
     }
 
     pub fn clear(self: *Focus) void {
-        self.rects.clearRetainingCapacity();
+        self.children.clearRetainingCapacity();
     }
 };
